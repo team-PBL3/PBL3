982029dba838c029ba11b7c65b047b0c130ba1d7
luongthien1 <94225197+luongthien1@users.noreply.github.com>
2022-05-17 00:08:03 +0700

    Thiện (Up Home+Login)
    Code phần Home và Sign in. Một phần của Sign up

---
+++ PBL3/PBL3/Controllers/Web/LoginController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Devart.Data.Linq;
+using Pbl3Context;
+namespace PBL3.Controllers.Web
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Sign_up()
+        {
+            //Pbl3DataContext dataContext = new Pbl3DataContext();
+            //List<User> dataUsers = dataContext.Users.ToList();
+            return View();
+        }
+        public ActionResult Sign_in(string email, string password)
+        {
+            Pbl3DataContext dataContext = new Pbl3DataContext();
+            List<User> dataUsers = dataContext.Users.ToList();
+            ViewBag.isAccess = false;
+            foreach (var i in dataUsers)
+                if (i.Email == email && i.Password == password) ViewBag.isAccess = true;
+                else ViewBag.isAccess = false;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Sign_up(User user)
+        {
+            return View();
+        }
+    }
+}
