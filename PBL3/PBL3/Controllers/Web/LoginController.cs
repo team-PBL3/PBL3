d30d01db739f873416b162d61a6148557cc23030
luongthien1 <94225197+luongthien1@users.noreply.github.com>
2022-05-17 19:13:20 +0700

    Thiện (Edit SignIn + Code SignUp)
    Sửa Sign In và code Sign up

--- PBL3/PBL3/Controllers/Web/LoginController.cs
+++ PBL3/PBL3/Controllers/Web/LoginController.cs
@@ -5,31 +5,138 @@ using System.Web;
 using System.Web.Mvc;
 using Devart.Data.Linq;
 using Pbl3Context;
+using PBL3.Models;
 namespace PBL3.Controllers.Web
 {
     public class LoginController : Controller
     {
+        Pbl3DataContext dataContext;
+        public LoginController()
+        {
+            dataContext = new Pbl3DataContext();
+        }    
         // GET: Login
         public ActionResult Sign_up()
         {
             //Pbl3DataContext dataContext = new Pbl3DataContext();
             //List<User> dataUsers = dataContext.Users.ToList();
+            SignUp_Errors a = new SignUp_Errors();
+            ViewBag.SignUp_Check = new SignUp_Errors();
             return View();
         }
+        public ActionResult Sign_in()
+        {
+            return View();
+        }   
+        [HttpPost]
         public ActionResult Sign_in(string email, string password)
         {
-            Pbl3DataContext dataContext = new Pbl3DataContext();
+            //Pbl3DataContext dataContext = new Pbl3DataContext();
             List<User> dataUsers = dataContext.Users.ToList();
             ViewBag.isAccess = false;
             foreach (var i in dataUsers)
-                if (i.Email == email && i.Password == password) ViewBag.isAccess = true;
+                if (i.Email == email && i.Password == password)
+                {
+                    ViewBag.isAccess = true;
+                    break;
+                }
                 else ViewBag.isAccess = false;
             return View();
         }
         [HttpPost]
-        public ActionResult Sign_up(User user)
+        public ActionResult Sign_up(User model, string cfpass)
         {
+            List<User> dataUsers = dataContext.Users.ToList();
+            SignUp_Errors error = new SignUp_Errors();
+            bool success = true;
+            if (model.Name == null)
+            {
+                success = false;
+                error.Name = "Please fill the field \" Name \"";
+            }
+            else
+            {
+                error.Name = "";
+            }
+
+            if (model.Sex == null)
+            {
+                success = false;
+                error.Sex = "Please select a value in field \" Sex \"";
+            }
+            else
+            {
+                error.Sex = "";
+            }
+
+            if (model.Address == null)
+            {
+                success = false;
+                error.Address = "Please fill the field \" Address \"";
+            }
+            else
+            {
+                error.Address = "";
+            }
+
+            if (model.Username == null)
+            {
+                success = false;
+                error.Username = "Please fill the field \" Username \"";
+            }
+            else
+            {
+                error.Username = "";
+                foreach (var data in dataUsers)
+                {
+                    if (data.Username==model.Username)
+                    {
+                        success = false;
+                        error.Username = $"User \"{model.Username}\" have existed. Please use another name";
+                        break;
+                    }    
+                }    
+            }
+            if (model.Email == null || !model.Email.Contains("@"))
+            {
+                success = false;
+                error.Email = "Invail email";
+            }
+            else
+            {
+                error.Email = "";
+            }
+            if (model.Password == null) model.Password = "";
+            if (model.Password.Length < 6)
+            {
+                success = false;
+                error.Password = "Length of password must larger than 6 characters.";
+            }
+            else
+            {
+                error.Password = "";
+            }
+            if (model.Password!=cfpass)
+            {
+                success = false;
+                error.Cfpassword = "The confirm password don't match password.";
+            }
+            else
+            {
+                error.Cfpassword = "";
+            }
+
+            if (success == false) error.result = "Sign up fail!";
+            else
+            {
+                dataContext.CreateNewUser(dataUsers.Count+1, model.Name, model.Sex, model.Phone, model.Address,
+                    model.Username, model.Email, model.Password, "INACTIVE", 2);
+                return View("Sign_in");
+            }    
+            ViewBag.SignUp_Check = error;
+
             return View();
         }
     }
+    
 }
---
+++ PBL3/PBL3/Models/Errors.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PBL3.Models
+{
+    public class Errors
+    {
+    }
+    public class SignUp_Errors
+    {
+        public string Name;
+        public string Sex;
+        public string Address;
+        public string Phone;
+        public string Username;
+        public string Email;
+        public string Password;
+        public string Cfpassword;
+        public string result;
+        public SignUp_Errors()
+        {
+            Name = "";
+            Sex = "";
+            Address = "";
+            Phone = "";
+            Username = "";
+            Email = "";
+            Password = "";
+            Cfpassword = "";
+            result = "";
+        }
+    }
+}
