20a8bf95c26d4fcb9e50fbcf984daa7a25753405
luongthien1 <94225197+luongthien1@users.noreply.github.com>
2022-05-27 21:27:27 +0700

    Thiện
    aaaa

---
+++ PBL_3/PBL_3/App_Start/BundleConfig.cs
@@ -0,0 +1,34 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+<<<<<<< Updated upstream:PBL_3/PBL_3/App_Start/BundleConfig.cs
+namespace PBL_3
+=======
+namespace PBL3
+>>>>>>> Stashed changes:PBL3/PBL3/App_Start/BundleConfig.cs
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.validate*"));
+
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.css",
+                      "~/Content/site.css"));
+        }
+    }
+}
---
+++ PBL_3/PBL_3/Models/Payment.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PBL3.Models
+{
+    public class Payment
+    {
+        public Payment()
+        {
+
+        }
+        public int id { get; set; }
+        [DataType(DataType.DateTime)]
+        public DateTime paymentdate { get; set; }
+        public int amount { get; set; }
+        public double totalPrice { get; set; }
+        public int orderid { get; set; }
+        public virtual Orderr order { get; set; }
+    }
+}
