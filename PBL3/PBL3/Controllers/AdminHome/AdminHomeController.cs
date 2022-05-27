65d5c522d8315cf2bc7643d0536de720b52e599d
luongthien1 <94225197+luongthien1@users.noreply.github.com>
2022-05-27 21:32:20 +0700

    Thiện
    Create

---
+++ PBL3/PBL3/Controllers/AdminHome/AdminHomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PBL3.Controllers.AdminHome
+{
+    public class AdminHomeController : RouteController
+    {
+        public static string Name = "AdminHome";
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
