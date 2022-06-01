b8391f7d630c560258dbc5eeec841d1c521354a4
luongthien1 <94225197+luongthien1@users.noreply.github.com>
2022-06-01 10:25:37 +0700

    Thiện

---
+++ PBL3/PBL3/Controllers/Member/CartController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PBL3.Models;
+namespace PBL3.Controllers.Member
+{
+    public class CartController : Controller
+    {
+        PBL3DataContext dataContext;
+        // GET: Cart
+        public CartController()
+        {
+            dataContext = new PBL3DataContext();
+        }    
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult AddToCart(int quantity_input, string product)
+        {
+
+            return View();
+        }    
+    }
+}
---
+++ PBL3/PBL3/PBL3/Migrations/202205311708395_AddDatetime2.cs
@@ -0,0 +1,18 @@
+﻿namespace PBL3.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddDatetime2 : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.CartDetails", "Time", c => c.DateTime(nullable: false, precision: 0));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.CartDetails", "Time");
+        }
+    }
+}
